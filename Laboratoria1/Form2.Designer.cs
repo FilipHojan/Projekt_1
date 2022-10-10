78640715b3e155e89afe95abc882292d9ea033e0
FilipHojan <81771162+FilipHojan@users.noreply.github.com>
2022-10-10 20:02:15 +0200

    Stoper cz1

--- Laboratoria1/Form2.Designer.cs
+++ Laboratoria1/Form2.Designer.cs
@@ -58,11 +58,11 @@ namespace Laboratoria1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(180, 61);
+            this.label1.Location = new System.Drawing.Point(124, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(425, 108);
+            this.label1.Size = new System.Drawing.Size(561, 108);
             this.label1.TabIndex = 2;
-            this.label1.Text = "00:00:00";
+            this.label1.Text = "00:00:00:00";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form2
--- Laboratoria1/Form2.cs
+++ Laboratoria1/Form2.cs
@@ -6,12 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Laboratoria1
 {
     public partial class Form2 : Form
     {
+        System.Timers.Timer timer;
+        int h, m, s, ms;
         public Form2()
         {
             InitializeComponent();
@@ -19,7 +22,33 @@ namespace Laboratoria1
 
         private void label1_Click(object sender, EventArgs e)
         {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1;
+            timer.Elapsed += TimeEvent;
+        }
 
+        private void TimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke (new Action(()=>{
+
+                ms += 1;
+                if(ms == 100){
+                    ms = 0;
+                    s += 1;
+                }
+                if(s == 60){
+                    s = 0;
+                    m += 1;
+                }
+                if(m == 60){
+                    m = 0;
+                    h += 1;
+                }
+
+                label1.Text = string.Format("{0}:{1}:{2}:{3}", h.ToString().ToString().PadLeft(2, '0'), m.ToString().ToString().PadLeft(2, '0'), s.ToString().ToString().PadLeft(2, '0'), ms.ToString().ToString().PadLeft(2, '0'));
+
+
+            }));
         }
     }
 }
